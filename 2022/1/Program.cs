edc69fa587c9dc0f5d3dd71b06c8f91e122ac6b3
Owen Daly-Furlong <owendalyfurlong@protonmail.com>
2023-02-11 19:43:08 +0000

    First challenge p1

---
+++ 2022/1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var input = await File.ReadAllTextAsync("Input.txt");
+
+var maxCalories = input
+    .Split("\n\n", StringSplitOptions.RemoveEmptyEntries)
+    .Select(x => x.Split("\n", StringSplitOptions.RemoveEmptyEntries))
+    .Select(x => x.Select(int.Parse))
+    .Select(x => x.Sum())
+    .Max();
+
+Console.WriteLine(maxCalories);
