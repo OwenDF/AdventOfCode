6c894fd5acebd095b85d8dc3c5e70499a78ecb98
Owen Daly-Furlong <owendalyfurlong@protonmail.com>
2023-06-11 12:12:16 +0100

    Day 9 part 2

--- 2022/9/Functions.cs
+++ 2022/9/Functions.cs
@@ -2,6 +2,11 @@ namespace _9;
 
 public static class Functions
 {
+    const char Up = 'U';
+    const char Down = 'D';
+    const char Right = 'R';
+    const char Left = 'L';
+    
     public static Instruction MapToInstruction(string input)
     {
         var split = input.Split(' ');
@@ -16,4 +21,90 @@ public static class Functions
 
     public static bool IsYAdjacentTo(this Point first, Point second) =>
         first.Y - second.Y is 0 or 1 or -1;
+
+    public static void MoveKnots(Instruction instruction, Point[] points, HashSet<Point> tailVisits)
+    {
+        var head = points[0];
+
+        for (var i = 0; i < instruction.MoveCount; i++)
+        {
+            switch (instruction.Direction)
+            {
+                case Up:
+                    head = head with { Y = head.Y + 1 };
+                    break;
+                case Down:
+                    head = head with { Y = head.Y - 1 };
+                    break;
+                case Left:
+                    head = head with { X = head.X - 1 };
+                    break;
+                case Right:
+                    head = head with { X = head.X + 1 };
+                    break;
+            }
+            points[0] = head;
+            var tailPosition = MoveTailKnots(points);
+            tailVisits.Add(tailPosition);
+        }
+    }
+    
+    private static Point MoveTailKnots(Span<Point> points)
+    {
+        if (points.Length is 1) return points[0];
+
+        var head = points[0];
+        var tail = points[1];
+        
+        if (tail.IsAdjacentTo(head)) goto end;
+        
+        if (tail.X == head.X)
+        {
+            if (tail.Y < head.Y)
+            {
+                tail = tail with { Y = tail.Y + 1 };
+                goto end;
+            }
+        
+            tail = tail with { Y = tail.Y - 1 };
+            goto end;
+        }
+        
+        if (tail.Y == head.Y)
+        {
+            if (tail.X < head.X)
+            {
+                tail = tail with { X = tail.X + 1 };
+                goto end;
+            }
+        
+            tail = tail with { X = tail.X - 1 };
+            goto end;
+        }
+        
+        if (tail.Y < head.Y && tail.X < head.X)
+        {
+            tail = tail with { X = tail.X + 1, Y = tail.Y + 1 };
+            goto end;
+        }
+        
+        if (tail.Y < head.Y && tail.X > head.X)
+        {
+            tail = tail with { X = tail.X - 1, Y = tail.Y + 1 };
+            goto end;
+        }
+        
+        if (tail.Y > head.Y && tail.X > head.X)
+        {
+            tail = tail with { X = tail.X - 1, Y = tail.Y - 1 };
+            goto end;
+        }
+        
+        tail = tail with { X = tail.X + 1, Y = tail.Y - 1 };
+        
+        end:
+        points[1] = tail;
+
+        return MoveTailKnots(points[1..]);
+    }
 }
--- 2022/9/Program.cs
+++ 2022/9/Program.cs
@@ -1,86 +1,14 @@
 ﻿using _9;
 using static _9.Functions;
 
-const char Up = 'U';
-const char Down = 'D';
-const char Right = 'R';
-const char Left = 'L';
-
 var instructions = (await File.ReadAllLinesAsync("Input.txt")).Select(MapToInstruction);
 
 var tailVisits = new HashSet<Point>();
-var head = new Point(1, 1);
-var tail = new Point(1, 1);
+var knots = new Point[10];
 
 foreach (var instruction in instructions)
 {
-    for (var i = 0; i < instruction.MoveCount; i++)
-    {
-        switch (instruction.Direction)
-        {
-            case Up:
-                head = head with { Y = head.Y + 1 };
-                break;
-            case Down:
-                head = head with { Y = head.Y - 1 };
-                break;
-            case Left:
-                head = head with { X = head.X - 1 };
-                break;
-            case Right:
-                head = head with { X = head.X + 1 };
-                break;
-        }
-        
-        tailVisits.Add(tail);
-        if (tail.IsAdjacentTo(head)) continue;
-        
-        if (tail.X == head.X)
-        {
-            if (tail.Y < head.Y)
-            {
-                tail = tail with { Y = tail.Y + 1 };
-                continue;
-            }
-        
-            tail = tail with { Y = tail.Y - 1 };
-            continue;
-        }
-        
-        if (tail.Y == head.Y)
-        {
-            if (tail.X < head.X)
-            {
-                tail = tail with { X = tail.X + 1 };
-                continue;
-            }
-        
-            tail = tail with { X = tail.X - 1 };
-            continue;
-        }
-        
-        if (tail.Y < head.Y && tail.X < head.X)
-        {
-            tail = tail with { X = tail.X + 1, Y = tail.Y + 1 };
-            continue;
-        }
-        
-        if (tail.Y < head.Y && tail.X > head.X)
-        {
-            tail = tail with { X = tail.X - 1, Y = tail.Y + 1 };
-            continue;
-        }
-        
-        if (tail.Y > head.Y && tail.X > head.X)
-        {
-            tail = tail with { X = tail.X - 1, Y = tail.Y - 1 };
-            continue;
-        }
-        
-        tail = tail with { X = tail.X + 1, Y = tail.Y - 1 };
-    }
+    MoveKnots(instruction, knots, tailVisits);
 }
-
-tailVisits.Add(tail);
 
 Console.WriteLine(tailVisits.Count);
