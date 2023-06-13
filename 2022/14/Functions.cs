630317b69d977ad94e139f4c29a874b6d4ca1c96
Owen Daly-Furlong <owendalyfurlong@protonmail.com>
2023-06-13 14:13:27 +0100

    Further clean up

--- 2022/14/Functions.cs
+++ 2022/14/Functions.cs
@@ -49,8 +49,8 @@ public static class Functions
 
     public static int GetTotalSandfall(HashSet<Point> blockers)
     {
-        var yMax = blockers.MaxBy(x => x.Y).Y + 1;
         var sandStart = new Point(500, 0);
+        var yMax = blockers.MaxBy(x => x.Y).Y + 1;
         
         for (var i = 0;;i++)
         {
@@ -59,40 +59,46 @@ public static class Functions
                 return i;
             }
 
-            var sandParticle = sandStart;
-            while (true)
+            AddSandParticle(blockers, sandStart, yMax);
+        }
+    }
+
+    private static void AddSandParticle(HashSet<Point> blockers, Point sandStart, int yMax)
+    {
+
+        var sandParticle = sandStart;
+        while (true)
+        {
+            if (sandParticle.Y == yMax)
             {
-                if (sandParticle.Y == yMax)
-                {
-                    blockers.Add(sandParticle);
-                    break;
-                }
-
-                var down = sandParticle with { Y = sandParticle.Y + 1 };
-                var downLeft = new Point(sandParticle.X - 1, sandParticle.Y + 1);
-                var downRight = new Point(sandParticle.X + 1, sandParticle.Y + 1);
-
-                if (!blockers.Contains(down))
-                {
-                    sandParticle = down;
-                    continue;
-                }
-
-                if (!blockers.Contains(downLeft))
-                {
-                    sandParticle = downLeft;
-                    continue;
-                }
-        
-                if (!blockers.Contains(downRight))
-                {
-                    sandParticle = downRight;
-                    continue;
-                }
-        
                 blockers.Add(sandParticle);
                 break;
             }
+
+            var down = sandParticle with { Y = sandParticle.Y + 1 };
+            var downLeft = new Point(sandParticle.X - 1, sandParticle.Y + 1);
+            var downRight = new Point(sandParticle.X + 1, sandParticle.Y + 1);
+
+            if (!blockers.Contains(down))
+            {
+                sandParticle = down;
+                continue;
+            }
+
+            if (!blockers.Contains(downLeft))
+            {
+                sandParticle = downLeft;
+                continue;
+            }
+        
+            if (!blockers.Contains(downRight))
+            {
+                sandParticle = downRight;
+                continue;
+            }
+        
+            blockers.Add(sandParticle);
+            break;
         }
     }
 }
