6df2f104d793d938c9481689ca9766f0593a7c10
Owen Daly-Furlong <owendalyfurlong@protonmail.com>
2023-06-18 21:25:48 +0100

    Day 19 part 2

--- 2022/19/Functions.cs
+++ 2022/19/Functions.cs
@@ -21,19 +21,19 @@ public static class Functions
 
     public static int FindMaxGeodes(
         State state,
-        Blueprint robotRecipes,
+        Blueprint blueprints,
         int currentTurn,
         int maxTurns,
         SkippedProduction skips)
     {
         if (currentTurn == maxTurns) return state.Resources.Geodes;
 
-        var canProduceOreRobot = state.Resources.Ore >= robotRecipes.OreRobotRecipe.OreCost;
-        var canProduceClayRobot = state.Resources.Ore >= robotRecipes.ClayRobotRecipe.OreCost;
-        var canProduceObsidianRobot = state.Resources.Ore >= robotRecipes.ObsidianRobotRecipe.OreCost &&
-                                      state.Resources.Clay >= robotRecipes.ObsidianRobotRecipe.ClayCost;
-        var canProduceGeodeRobot = state.Resources.Ore >= robotRecipes.GeodeRobotRecipe.OreCost &&
-                                   state.Resources.Obsidian >= robotRecipes.GeodeRobotRecipe.ObsidianCost;
+        var canProduceOreRobot = state.Resources.Ore >= blueprints.OreRobotRecipe.OreCost;
+        var canProduceClayRobot = state.Resources.Ore >= blueprints.ClayRobotRecipe.OreCost;
+        var canProduceObsidianRobot = state.Resources.Ore >= blueprints.ObsidianRobotRecipe.OreCost &&
+                                      state.Resources.Clay >= blueprints.ObsidianRobotRecipe.ClayCost;
+        var canProduceGeodeRobot = state.Resources.Ore >= blueprints.GeodeRobotRecipe.OreCost &&
+                                   state.Resources.Obsidian >= blueprints.GeodeRobotRecipe.ObsidianCost;
 
         var produceNothing = FindMaxGeodes(
             state with
@@ -44,7 +44,7 @@ public static class Functions
                     state.Resources.Obsidian + state.Robots.ObsidianRobots,
                     state.Resources.Geodes + state.Robots.GeodeRobots)
             },
-            robotRecipes,
+            blueprints,
             currentTurn + 1,
             maxTurns,
             new(canProduceOreRobot, canProduceClayRobot, canProduceObsidianRobot, canProduceGeodeRobot));
@@ -53,53 +53,59 @@ public static class Functions
         var produceClayRobot = 0;
         var produceObsidianRobot = 0;
         var produceGeodeRobot = 0;
+        
+        if (!skips.GeodeRobot && canProduceGeodeRobot)
+        {
+            return FindMaxGeodes(
+                new State(new Resources(
+                        state.Resources.Ore + state.Robots.OreRobots - blueprints.GeodeRobotRecipe.OreCost,
+                        state.Resources.Clay + state.Robots.ClayRobots,
+                        state.Resources.Obsidian + state.Robots.ObsidianRobots - blueprints.GeodeRobotRecipe.ObsidianCost,
+                        state.Resources.Geodes + state.Robots.GeodeRobots),
+                    state.Robots with { GeodeRobots = state.Robots.GeodeRobots + 1 }),
+                blueprints, currentTurn + 1, maxTurns, SkippedProduction.None);
+        }
 
-        if (!skips.OreRobot && canProduceOreRobot)
+        if (!skips.OreRobot &&
+            canProduceOreRobot &&
+            state.Robots.OreRobots < Math.Max(
+                Math.Max(
+                    Math.Max(blueprints.OreRobotRecipe.OreCost, blueprints.ClayRobotRecipe.OreCost),
+                    blueprints.ObsidianRobotRecipe.OreCost),
+                blueprints.GeodeRobotRecipe.OreCost))
         {
             produceOreRobot = FindMaxGeodes(
                 new State(new Resources(
-                        state.Resources.Ore + state.Robots.OreRobots - robotRecipes.OreRobotRecipe.OreCost,
+                        state.Resources.Ore + state.Robots.OreRobots - blueprints.OreRobotRecipe.OreCost,
                         state.Resources.Clay + state.Robots.ClayRobots,
                         state.Resources.Obsidian + state.Robots.ObsidianRobots,
                         state.Resources.Geodes + state.Robots.GeodeRobots),
                     state.Robots with { OreRobots = state.Robots.OreRobots + 1 }),
-                robotRecipes, currentTurn + 1, maxTurns, SkippedProduction.None);
+                blueprints, currentTurn + 1, maxTurns, SkippedProduction.None);
         }
         
-        if (!skips.ClayRobot && canProduceClayRobot)
+        if (!skips.ClayRobot && canProduceClayRobot && state.Robots.ClayRobots < blueprints.ObsidianRobotRecipe.ClayCost)
         {
             produceClayRobot = FindMaxGeodes(
                 new State(new Resources(
-                        state.Resources.Ore + state.Robots.OreRobots - robotRecipes.ClayRobotRecipe.OreCost,
+                        state.Resources.Ore + state.Robots.OreRobots - blueprints.ClayRobotRecipe.OreCost,
                         state.Resources.Clay + state.Robots.ClayRobots,
                         state.Resources.Obsidian + state.Robots.ObsidianRobots,
                         state.Resources.Geodes + state.Robots.GeodeRobots),
                     state.Robots with { ClayRobots = state.Robots.ClayRobots + 1 }),
-                robotRecipes, currentTurn + 1, maxTurns, SkippedProduction.None);
+                blueprints, currentTurn + 1, maxTurns, SkippedProduction.None);
         }
         
         if (!skips.ObsidianRobot && canProduceObsidianRobot)
         {
             produceObsidianRobot = FindMaxGeodes(
                 new State(new Resources(
-                        state.Resources.Ore + state.Robots.OreRobots - robotRecipes.ObsidianRobotRecipe.OreCost,
-                        state.Resources.Clay + state.Robots.ClayRobots - robotRecipes.ObsidianRobotRecipe.ClayCost,
+                        state.Resources.Ore + state.Robots.OreRobots - blueprints.ObsidianRobotRecipe.OreCost,
+                        state.Resources.Clay + state.Robots.ClayRobots - blueprints.ObsidianRobotRecipe.ClayCost,
                         state.Resources.Obsidian + state.Robots.ObsidianRobots,
                         state.Resources.Geodes + state.Robots.GeodeRobots),
                     state.Robots with { ObsidianRobots = state.Robots.ObsidianRobots + 1 }),
-                robotRecipes, currentTurn + 1, maxTurns, SkippedProduction.None);
-        }
-        
-        if (!skips.GeodeRobot && canProduceGeodeRobot)
-        {
-            produceGeodeRobot = FindMaxGeodes(
-                new State(new Resources(
-                        state.Resources.Ore + state.Robots.OreRobots - robotRecipes.GeodeRobotRecipe.OreCost,
-                        state.Resources.Clay + state.Robots.ClayRobots,
-                        state.Resources.Obsidian + state.Robots.ObsidianRobots - robotRecipes.GeodeRobotRecipe.ObsidianCost,
-                        state.Resources.Geodes + state.Robots.GeodeRobots),
-                    state.Robots with { GeodeRobots = state.Robots.GeodeRobots + 1 }),
-                robotRecipes, currentTurn + 1, maxTurns, SkippedProduction.None);
+                blueprints, currentTurn + 1, maxTurns, SkippedProduction.None);
         }
 
         return new[] { produceNothing, produceOreRobot, produceClayRobot, produceObsidianRobot, produceGeodeRobot }
--- 2022/19/Program.cs
+++ 2022/19/Program.cs
@@ -1,8 +1,6 @@
 ﻿using _19;
 using static _19.Functions;
 
-var recipe = new Blueprint(new(4), new(2), new(3, 14), new(2, 7));
-
 var recipes = (await File.ReadAllLinesAsync("Input.txt")).Select(ToBlueprint).ToList();
 var initialState = new State(new Resources(0, 0, 0, 0), new Robots(1, 0, 0, 0));
 
@@ -12,4 +10,11 @@ for (var i = 0; i < recipes.Count; i++)
     totalQuality += (i + 1) * FindMaxGeodes(initialState, recipes[i], 0, 24, SkippedProduction.None);
 }
 
+var score = 1;
+foreach (var recipe in recipes.Take(3))
+{
+    score *= FindMaxGeodes(initialState, recipe, 0, 32, SkippedProduction.None);
+}
+
 Console.WriteLine(totalQuality);
+Console.WriteLine(score);
