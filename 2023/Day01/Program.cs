a6d651f7cf0b297c2931a78b1984b93a4073d231
Owen Daly-Furlong <owendalyfurlong@protonmail.com>
2023-12-02 10:40:57 +0000

    Day 1 Part 1

---
+++ 2023/Day01/Program.cs
@@ -0,0 +1,5 @@
+﻿var lines = File.ReadAllLines("Input.txt");
+
+var total = lines.Select(x => int.Parse(x.First(char.IsAsciiDigit).ToString() + x.Last(char.IsAsciiDigit))).Sum();
+
+Console.WriteLine(total);
