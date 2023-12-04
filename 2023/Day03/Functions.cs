c44017ff2d33e3589ef70f022b549e21cde0c19d
Owen Daly-Furlong <owendalyfurlong@protonmail.com>
2023-12-04 21:35:48 +0000

    Day 3 Part 2

--- 2023/Day03/Functions.cs
+++ 2023/Day03/Functions.cs
@@ -15,8 +15,28 @@ internal static class Functions
 
         return false;
     }
+
+    public static int[] GetAdjacentNumbers(Point p, Dictionary<Point, Box<int>> numbers) =>
+        new[]
+        {
+            numbers.TryGetValue(new Point(p.X - 1, p.Y + 1), out var a) ? a : null,
+            numbers.TryGetValue(new Point(p.X + 1, p.Y - 1), out var b) ? b : null,
+            numbers.TryGetValue(new Point(p.X + 1, p.Y + 1), out var c) ? c : null,
+            numbers.TryGetValue(new Point(p.X - 1, p.Y - 1), out var d) ? d : null,
+            numbers.TryGetValue(p with { X = p.X - 1 }, out var e) ? e : null,
+            numbers.TryGetValue(p with { X = p.X + 1 }, out var f) ? f : null,
+            numbers.TryGetValue(p with { Y = p.Y - 1 }, out var g) ? g : null,
+            numbers.TryGetValue(p with { Y = p.Y + 1 }, out var h) ? h : null,
+        }.Where(x => x is not null).GroupBy(x => x).Select(x => x.Key!.Value).ToArray();
+
+    public static int ToNum(this char c) => int.Parse(c.ToString());
     
     public static bool IsPartSymbol(this char c) => !(c is '.' || char.IsDigit(c));
 }
 
 public record struct Point(int X, int Y);
+
+public class Box<T>(T value)
+{
+    public T Value { get; set; } = value;
+}
--- 2023/Day03/Program.cs
+++ 2023/Day03/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Day03;
+using static Day03.Functions;
 
 var lines = File.ReadAllLines("Input.txt");
 
@@ -7,28 +8,34 @@ var grid = new char[width, lines.Length];
 
 for (var i = 0; i < width; i++) for (var j = 0; j < lines.Length; j++) grid[i, j] = lines[j][i];
 
-var sum = 0;
+var numbers = new Dictionary<Point, Box<int>>();
 
 for (var y = 0; y < lines.Length; y++)
 {
-    var partNum = 0;
-    var isPartNum = false;
+    Box<int>? partNum = null;
     for (var x = 0; x < width; x++)
     {
         if (!char.IsDigit(grid[x, y]))
         {
-            if (isPartNum) sum += partNum;
-            isPartNum = false;
-            partNum = 0;
+            partNum = null;
             continue;
         }
 
-        isPartNum = isPartNum || new Point(x, y).IsPartNumber(grid, width - 1, lines.Length - 1);
-        partNum *= 10;
-        partNum += int.Parse(grid[x, y].ToString());
-    }
+        if (partNum is null) partNum = new Box<int>(0);
 
-    if (isPartNum) sum += partNum;
+        partNum.Value = partNum.Value * 10 + grid[x, y].ToNum();
+        numbers[new(x, y)] = partNum;
+    }
+}
+
+var sum = 0;
+
+for (var x = 0; x < width; x++) for (var y = 0; y < lines.Length; y++)
+{
+    if (grid[x, y] is not '*') continue;
+
+    var adjacentNumbers = GetAdjacentNumbers(new(x, y), numbers);
+    if (adjacentNumbers.Length is 2) sum += adjacentNumbers[0] * adjacentNumbers[1];
 }
 
 Console.WriteLine(sum);
