470e84b6512945dac53a1f434c8aa40ff0bf57a8
Owen Daly-Furlong <owendalyfurlong@protonmail.com>
2023-12-05 20:22:04 +0000

    Day 5 Part 2

--- 2023/Day05/Functions.cs
+++ 2023/Day05/Functions.cs
@@ -5,24 +5,72 @@ internal static class Functions
     public static MapList ToMapList(string[] section)
     {
         var names = section[0].Split(' ')[0].Split('-');
-        return new MapList(names[0], names[2], section.Skip(1).Select(ToMap).ToList());
+        return new MapList(names[0], names[2], section.Skip(1).Select(ToMap)
+            // Filthy hack
+            .Append(new(new(long.MaxValue - 1, long.MaxValue), 0))
+            .OrderBy(x => x.SourceRange.InclusiveStart).ToArray());
     }
 
-    public static long ConvertToValue(
-        this long val,
+    public static IEnumerable<Range> CreateRanges(this IEnumerable<long> nums)
+    {
+        using var enumerator = nums.GetEnumerator();
+
+        while (enumerator.MoveNext())
+        {
+            var first = enumerator.Current;
+            enumerator.MoveNext();
+            var second = enumerator.Current;
+            yield return new Range(first, first + second);
+        }
+    }
+
+    public static IReadOnlyList<Range> ConvertToDestinationRanges(
+        this IReadOnlyList<Range> ranges,
         Dictionary<string, MapList> mapLists,
         string mapName,
         string lastMapName)
     {
-        if (mapName == lastMapName) return val;
+        if (mapName == lastMapName) return ranges;
 
         var mapList = mapLists[mapName];
-        var map = mapList.Maps.SingleOrDefault(x => val.InRange(x.SourceRange));
-        val += map?.DestinationOffset ?? 0;
+        var convertedRanges = ranges.Select(r => GetDestinationRanges(mapList.Maps, r)).SelectMany(x => x).ToList();
 
-        return ConvertToValue(val, mapLists, mapList.ToType, lastMapName);
+        return ConvertToDestinationRanges(convertedRanges, mapLists, mapList.ToType, lastMapName);
+    }
+
+    private static IEnumerable<Range> GetDestinationRanges(Span<Map> maps, Range sourceRange)
+    {
+        if (sourceRange.ExclusiveEnd <= sourceRange.InclusiveStart) return Enumerable.Empty<Range>();
+
+        var map = maps[0];
+
+        if (sourceRange.InclusiveStart < map.SourceRange.InclusiveStart)
+        {
+            return GetDestinationRanges(maps, sourceRange with { InclusiveStart = map.SourceRange.InclusiveStart })
+                .Append(sourceRange with
+                {
+                    ExclusiveEnd = Math.Min(map.SourceRange.InclusiveStart, sourceRange.ExclusiveEnd)
+                });
+        }
+
+        if (sourceRange.InclusiveStart >= map.SourceRange.ExclusiveEnd)
+            return GetDestinationRanges(maps[1..], sourceRange);
+
+        if (sourceRange.InclusiveStart >= map.SourceRange.InclusiveStart)
+        {
+            return GetDestinationRanges(maps[1..], sourceRange with { InclusiveStart = map.SourceRange.ExclusiveEnd })
+                .Append((sourceRange with
+                {
+                    ExclusiveEnd = Math.Min(sourceRange.ExclusiveEnd, map.SourceRange.ExclusiveEnd)
+                }).Offset(map.DestinationOffset));
+        }
+
+        throw new Exception("unreachable");
     }
     
+    private static Range Offset(this Range src, long offSet) 
+        => new(src.InclusiveStart + offSet, src.ExclusiveEnd + offSet);
+
     private static bool InRange(this long num, Range range) => num >= range.InclusiveStart && num < range.ExclusiveEnd;
     
     private static Map ToMap(string data)
@@ -36,4 +84,4 @@ internal static class Functions
 
 internal record struct Range(long InclusiveStart, long ExclusiveEnd);
 internal record Map(Range SourceRange, long DestinationOffset);
-internal record MapList(string FromType, string ToType, IReadOnlyList<Map> Maps);
+internal record MapList(string FromType, string ToType, Map[] Maps);
--- 2023/Day05/Program.cs
+++ 2023/Day05/Program.cs
@@ -4,7 +4,10 @@ using static Day05.Functions;
 var sections = File.ReadAllText("Input.txt").Split("\n\n")
     .Select(x => x.Split('\n', StringSplitOptions.RemoveEmptyEntries)).ToList();
 
-var seeds = sections[0][0].Split(':')[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToList();
+var seedRanges = sections[0][0].Split(':')[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(long.Parse)
+    .CreateRanges().ToList();
+
 var maps = sections.Skip(1).Select(ToMapList).ToDictionary(x => x.FromType, x => x);
 
-Console.WriteLine(seeds.Select(s => s.ConvertToValue(maps, "seed", "location")).Min());
+Console.WriteLine(seedRanges.ConvertToDestinationRanges(maps, "seed", "location").MinBy(x => x.InclusiveStart)
+    .InclusiveStart);
