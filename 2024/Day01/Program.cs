ef38a6318b5004f4a222af194bf53dcceb963b59
Owen Daly-Furlong <owendalyfurlong@protonmail.com>
2024-12-03 22:49:50 +0000

    Day 1 Part 1

---
+++ 2024/Day01/Program.cs
@@ -0,0 +1,17 @@
+﻿var lines = File.ReadAllLines("Input.txt");
+
+var (left, right) = (new List<int>(), new List<int>());
+foreach (var line in lines)
+{
+    var parts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    left.Add(int.Parse(parts[0]));
+    right.Add(int.Parse(parts[1]));
+}
+
+left.Sort();
+right.Sort();
+
+var total = 0;
+for (var i = 0; i < left.Count; i++) total += Math.Abs(left[i] - right[i]);
+
+Console.WriteLine(total);
