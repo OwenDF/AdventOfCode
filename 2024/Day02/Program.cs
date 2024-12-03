e72543066971f39a72bca064552d5853827c7a7b
Owen Daly-Furlong <owendalyfurlong@protonmail.com>
2024-12-03 23:36:52 +0000

    Day 2 Part 2

--- 2024/Day02/Program.cs
+++ 2024/Day02/Program.cs
@@ -1,9 +1,20 @@
 ﻿Console.WriteLine(File.ReadAllLines("Input.txt")
     .Select(x => x.Split(' '))
     .Select(x => x.Select(int.Parse).ToList())
-    .Select(ToDiffList)
+    .Select(ToDiffLists)
     .Select(x => x.ToList())
-    .Count(x => x.All(y => y is 1 or 2 or 3) || x.All(y => y is -1 or -2 or -3)));
+    .Count(x => x.Any(y => y.All(z => z is 1 or 2 or 3)) || x.Any(y => y.All(z => z is -1 or -2 or -3))));
+
+IEnumerable<IEnumerable<int>> ToDiffLists(IList<int> arr)
+{
+    yield return ToDiffList(arr);
+    for (var i = 0; i < arr.Count; i++)
+    {
+        var copyList = new List<int>(arr);
+        copyList.RemoveAt(i);
+        yield return ToDiffList(copyList);
+    }
+}
 
 IEnumerable<int> ToDiffList(IList<int> arr)
 {
