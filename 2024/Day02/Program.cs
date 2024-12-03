13e87b170b6bd57ddae0a7ac7ee5e85eb2ce6443
Owen Daly-Furlong <owendalyfurlong@protonmail.com>
2024-12-03 23:14:05 +0000

    Day 2 Part 1

---
+++ 2024/Day02/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine(File.ReadAllLines("Input.txt")
+    .Select(x => x.Split(' '))
+    .Select(x => x.Select(int.Parse).ToList())
+    .Select(ToDiffList)
+    .Select(x => x.ToList())
+    .Count(x => x.All(y => y is 1 or 2 or 3) || x.All(y => y is -1 or -2 or -3)));
+
+IEnumerable<int> ToDiffList(IList<int> arr)
+{
+    for (var i = 1; i < arr.Count; i++) yield return arr[i] - arr[i - 1];
+}
