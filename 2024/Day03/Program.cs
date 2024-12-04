b10571e7e7f42cd47bd1fa9219f73788338f1829
Owen Daly-Furlong <owendalyfurlong@protonmail.com>
2024-12-04 00:19:44 +0000

    Day 3 Part 1

---
+++ 2024/Day03/Program.cs
@@ -0,0 +1,54 @@
+﻿var textSpan = File.ReadAllText("Input.txt").AsSpan();
+
+long sum = 0;
+while (true)
+{
+    var possibleInstructionStart = textSpan.IndexOf("mul(");
+    if (possibleInstructionStart is -1) break;
+
+    textSpan = textSpan[(possibleInstructionStart + 4)..];
+
+    int firstPart;
+    if (int.TryParse(textSpan[..3], out var num3))
+    {
+        firstPart = num3;
+        textSpan = textSpan[3..];
+    }
+    else if (int.TryParse(textSpan[..2], out var num2))
+    {
+        firstPart = num2;
+        textSpan = textSpan[2..];
+    }
+    else if (int.TryParse(textSpan[..1], out var num1))
+    {
+        firstPart = num1;
+        textSpan = textSpan[1..];
+    }
+    else continue;
+
+    if (textSpan[0] is not ',') continue;
+    
+    textSpan = textSpan[1..];
+    
+    int secondPart;
+    if (int.TryParse(textSpan[..3], out var num23))
+    {
+        secondPart = num23;
+        textSpan = textSpan[3..];
+    }
+    else if (int.TryParse(textSpan[..2], out var num22))
+    {
+        secondPart = num22;
+        textSpan = textSpan[2..];
+    }
+    else if (int.TryParse(textSpan[..1], out var num21))
+    {
+        secondPart = num21;
+        textSpan = textSpan[1..];
+    }
+    else continue;
+    
+    if (textSpan[0] is ')') sum += firstPart * secondPart;
+}
+
+Console.WriteLine(sum);
