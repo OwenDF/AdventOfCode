7411c4047536b12168cc6b12200dfd5e9b24a799
Owen Daly-Furlong <owendalyfurlong@protonmail.com>
2024-12-06 19:40:32 +0000

    Day 5 Part 1

---
+++ 2024/Day05/Program.cs
@@ -0,0 +1,42 @@
+﻿var sections = File.ReadAllText("Input.txt").Split("\n\n", StringSplitOptions.RemoveEmptyEntries);
+
+var middleSum = 0;
+var rules = CreateRules(sections[0]);
+
+var printingLists = sections[1].Split('\n', StringSplitOptions.RemoveEmptyEntries)
+    .Select(x => x.Split(',', StringSplitOptions.RemoveEmptyEntries))
+    .Select(x => x.Select(int.Parse).ToList()).ToList();
+
+foreach (var list in printingLists)
+{
+    var items = new HashSet<int>();
+    foreach (var number in list)
+    {
+        items.Add(number);
+        if (rules[number].Intersect(items).Any()) goto end;
+    }
+
+    middleSum += list[list.Count / 2];
+    
+    end: ;
+}
+
+Console.WriteLine(middleSum);
+
+Dictionary<int, ISet<int>> CreateRules(string rules)
+{
+    var ruleItems = rules.Split('\n', StringSplitOptions.RemoveEmptyEntries)
+        .Select(x => x.Split('|'))
+        .Select(x => (int.Parse(x[0]), int.Parse(x[1])));
+
+    var result = new Dictionary<int, ISet<int>>();
+    
+    foreach (var (left, right) in ruleItems)
+    {
+        var list = result.TryGetValue(left, out var l) ? l : new HashSet<int>();
+        list.Add(right);
+        result[left] = list;
+    }
+
+    return result;
+}
